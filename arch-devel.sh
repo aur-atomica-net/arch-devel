acecd59271d4bd8a222cbc03a99e392bd68014e1
Jason R. McNeil <jason@jasonrm.net>
2016-04-08 01:17:23 -0700

    Inside the docker images we should delete cached packages

--- arch-devel.sh
+++ arch-devel.sh
@@ -3,7 +3,8 @@ set -e
 set -x
 set -o pipefail
 
-pacman -Suy --noconfirm base-devel git
+pacman -Suy --noconfirm --needed base-devel git
+pacman -Scc --noconfirm
 
 # Don't need this script anymore
 rm $0
