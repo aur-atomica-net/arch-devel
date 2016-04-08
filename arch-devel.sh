fce50311ee4810adc9c49fe468012cfeb64a8715
Jason R. McNeil <jason@jasonrm.net>
2016-04-08 00:48:18 -0700

    Drop pgp for now

--- arch-devel.sh
+++ arch-devel.sh
@@ -5,11 +5,5 @@ set -o pipefail
 
 pacman -Suy --noconfirm base-devel git
 
-# Should allow for packages to verify sources via GPG signatures
-mkdir -p /home/code_executor/.gnupg
-echo 'keyserver hkp://pool.sks-keyservers.net' > /home/code_executor/.gnupg/gpg.conf
-echo 'keyserver-options auto-key-retrieve' >> /home/code_executor/.gnupg/gpg.conf
-chown -R code_executor:code_executor /home/code_executor
-
 # Don't need this script anymore
 rm $0
