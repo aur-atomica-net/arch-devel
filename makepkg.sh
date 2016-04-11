cf7f49108034de9829ffaf6a57161cb58bdca560
Jason R. McNeil <jason@jasonrm.net>
2016-04-11 00:37:28 -0700

    Attempt to address gpg signing issue
    Might be https://wiki.archlinux.org/index.php/Makepkg#Makepkg_sometimes_fails_to_sign_a_package_without_asking_for_signature_passphrase

--- makepkg.sh
+++ makepkg.sh
@@ -6,10 +6,11 @@ set -o pipefail
 REPO=$1
 GNUPGHOME=$(pwd)/.gnupg
 
+gpg-agent
+
 makepkg --force --noconfirm --syncdeps --install --nocheck --sign
 
-repo-add ${REPO}.db.tar.gz *.pkg.tar.xz
-gpg --detach-sign ${REPO}.db.tar.gz
+repo-add --sign ${REPO}.db.tar.gz *.pkg.tar.xz
 
 mv ${REPO}.db.tar.gz ${REPO}.db
 mv ${REPO}.db.tar.gz.sig ${REPO}.db.sig
