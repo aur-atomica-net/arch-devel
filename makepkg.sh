adc6517136d03af7c8c1e07d7cac976636edade6
Jason R. McNeil <jason@jasonrm.net>
2016-04-11 00:28:01 -0700

    Try seperate signing process

--- makepkg.sh
+++ makepkg.sh
@@ -6,9 +6,10 @@ set -o pipefail
 REPO=$1
 GNUPGHOME=$(pwd)/.gnupg
 
-makepkg --force --noconfirm --syncdeps --install --nocheck --sign --key ${GPGKEY}
+makepkg --force --noconfirm --syncdeps --install --nocheck --sign
 
-repo-add --sign --key ${GPGKEY} ${REPO}.db.tar.gz *.pkg.tar.xz
+repo-add ${REPO}.db.tar.gz *.pkg.tar.xz
+gpg --detach-sign ${REPO}.db.tar.gz
 
 mv ${REPO}.db.tar.gz ${REPO}.db
 mv ${REPO}.db.tar.gz.sig ${REPO}.db.sig
