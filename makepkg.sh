28d55dffed92fcf04de577031ad3226023ee61cf
Jason R. McNeil <jason@jasonrm.net>
2016-04-10 23:47:29 -0700

    Don't verify for now

--- makepkg.sh
+++ makepkg.sh
@@ -7,7 +7,7 @@ REPO=$1
 
 makepkg --force --noconfirm --syncdeps --install --nocheck --sign --key ${GPGKEY}
 
-repo-add --sign --verify --key ${GPGKEY} ${REPO}.db.tar.gz *.pkg.tar.xz
+repo-add --sign --key ${GPGKEY} ${REPO}.db.tar.gz *.pkg.tar.xz
 
 mv ${REPO}.db.tar.gz ${REPO}.db
 mv ${REPO}.db.tar.gz.sig ${REPO}.db.sig
