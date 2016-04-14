04aa1f9505a68066a001377d5f1451eb5825874e
Jason R. McNeil <jason@jasonrm.net>
2016-04-14 08:13:43 -0700

    pull latest image

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 IMAGE_NAME="atomica/arch-devel"
 
-docker build --force-rm --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" --tag="${IMAGE_NAME}:latest" .
+docker build --pull --force-rm --build-arg http_proxy="${http_proxy}" --build-arg https_proxy="${https_proxy}" --tag="${IMAGE_NAME}:latest" .
 
 # Push to registry if configured
 if [ ! -z "${DOCKER_REGISTRY}" ]; then
