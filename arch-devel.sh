2cb976d82e65a1db245ad38b02d37e550e2fb9e6
Jason R. McNeil <github@jason.hckr.net>
2017-03-15 04:29:10 -0700

    Install ccache

--- arch-devel.sh
+++ arch-devel.sh
@@ -4,7 +4,7 @@ set -x
 set -o pipefail
 
 pacman --sync --refresh --noconfirm archlinux-keyring
-pacman --sync --sysupgrade --noconfirm --needed base base-devel git
+pacman --sync --sysupgrade --noconfirm --needed base base-devel git ccache
 pacman -Scc --noconfirm
 
 # Don't need this script anymore
