5d6d9c9a2f76a151b7b4a62f4b9c4b1495aa9b44
Jason R. McNeil <jason@jasonrm.net>
2018-07-16 14:08:07 -0700

    Keep proxy on sudo

--- arch-devel.sh
+++ arch-devel.sh
@@ -3,7 +3,8 @@ set -e
 set -x
 set -o pipefail
 
-pacman --sync --refresh --noconfirm archlinux-keyring
+echo 'Defaults env_keep += "http_proxy https_proxy ftp_proxy"' | sudo tee --append /etc/sudoers > /dev/null
+
 pacman --sync --sysupgrade --noconfirm --needed base base-devel git ccache
 pacman -Scc --noconfirm
 
