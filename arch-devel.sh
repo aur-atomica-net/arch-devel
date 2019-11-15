6c1e65db9d6814e1523d66c17b0c2d10806097d3
Jason R. McNeil <jason@mcneil.dev>
2019-11-15 12:36:20 -0700

    Always install linux

--- arch-devel.sh
+++ arch-devel.sh
@@ -6,7 +6,7 @@ set -o pipefail
 echo 'Defaults env_keep += "http_proxy https_proxy ftp_proxy"' | sudo tee --append /etc/sudoers > /dev/null
 
 pacman --sync --refresh --noconfirm --needed archlinux-keyring
-pacman --sync --sysupgrade --noconfirm --needed base base-devel git ccache
+pacman --sync --sysupgrade --noconfirm --needed base base-devel git ccache linux
 pacman -Scc --noconfirm
 
 # Don't need this script anymore
